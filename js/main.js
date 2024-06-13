8849f22b956c5d5f2e74fdebe9d285c113f5153a
asqarovilhomjon <asqarovilhomjon045@gmail.com>
2024-06-13 11:12:07 +0500

    aliexpress

---
+++ js/main.js
@@ -0,0 +1,60 @@
+//     1-masala
+// {
+//     let a = +prompt("a uchun son kiriting")
+//     alert(a * 4)
+// }
+
+//     2-masala
+// {
+//     let b = prompt("b uchun son kiriting")
+//     alert(b * b)
+// }
+
+//     3- masala
+// {
+//     let a = +prompt("enini kiriting") 
+//     let b = +prompt(" boyini kiriting") 
+//     alert("Yuzasi   " +( a * b ) +  "       Perimetri   " + (2 *(a + b)))
+// }
+
+//     4-masala
+// {
+//     let d = prompt("diametrini kiriting")
+//     p = 3.14
+//     alert(" Uzunligi  " + p * d)
+// }
+
+//     5-masala
+// {
+//     let a = +prompt(" Kubokning yon tomonini kiriting ")
+//     let v =  a * a * a
+//     let s =  6 *( a *a)
+//     alert("Hajmi  " + v  + " Tola sirti  "   +   s)
+// }
+
+//     6- masala
+// {
+//     let a = +prompt("Paralelepepedning a sonini kiriting")
+//     let b = +prompt("Paralelepepedning b sonini kiriting")
+//     let c = +prompt("Paralelepepedning c sonini kiriting")
+//     let v = a * b * c
+//     let s = 2 * (a * b + b * c + a * c)
+//     alert(" Uning hajmi  " + v  + "  Tola sirti  "  +  s)
+// }
+
+//     7-masala
+// {
+//     let r = prompt( "Doiraning radiusini kiriting ")
+//     let p = 3.14
+//     let l = 2 * p * r
+//     let s = p * (r * r)
+//     alert("  Uning uzunligi " + l  +  " Uning yuzasi  " + s)
+// }
+
+//     8-masala
+// {
+//     let a = +prompt(" a ga son kiriting ")
+//     let b = +prompt(" b ga son kiriting ")
+//     let c = (a + b )/2
+//     alert( " Orta arifmetigi " + c )
+// }
